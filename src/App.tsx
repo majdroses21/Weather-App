e27995a0005c6953e8a42ba2657e4397060c7778
Majd Roses <majdrose@gmail.com>
2026-01-23 22:53:01 +0300

    Added Shadcn/ui With Test Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./assets/css/App.css";
 import MainLayout from "./layout/Main";
+import Test from "./components/Test";
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <MainLayout>
         <h1>Vite + React</h1>
+        <br/>
+        <h1>Vite + React</h1>
+        <Test/>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button>Click me</Button>
+        </div>
+ 
+ 
       </MainLayout>
     </>
   );
