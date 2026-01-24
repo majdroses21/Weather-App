a6a6cc4e8b775adbae66b8b365e2b4cc46fad77d
Majd Roses <majdrose@gmail.com>
2026-01-24 17:40:59 +0300

    Fix layout bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import "./assets/css/App.css";
 import MainLayout from "./layout/Main";
 import Test from "./components/Test";
-import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <MainLayout>
         <h1>Vite + React</h1>
-        <br/>
+        <br />
         <h1>Vite + React</h1>
-        <Test/>
-        <div className="flex min-h-svh flex-col items-center justify-center">
-          <Button>Click me</Button>
-        </div>
- 
- 
+        <Test />
       </MainLayout>
     </>
   );
