b50ca6cd08e84404684c14dc601a926e3f6344c3
Majd Roses <majdrose@gmail.com>
2026-01-27 18:35:40 +0300

    Added bg to Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,32 @@
+const Links = [
+  {
+    title: "Projects",
+    link: "/projects",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+];
 const Footer = () => {
   return (
     <>
-      <h1>Footer</h1>
+      <footer className="footer bg-gray-400 shadow-2xl flex h-14 flexc">
+        <h1>Footer</h1>
+        {Links.map((ln, i) => (
+          <a
+            key={i}
+            href={ln.link}
+            className="text-sm font-medium hover:text-blue-600"
+          >
+            {ln.title}
+          </a>
+        ))}
+      </footer>
     </>
   );
 };
